4e0c57d90daab240d336f3f17c633a59b04cf90a
LucasLacerda95 <lucaslacerda561@gmail.com>
2022-10-27 08:34:23 -0300

    First Commit

---
+++ View/F_Login.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Ballance {
+    partial class F_Login {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_user = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // tb_user
+            // 
+            this.tb_user.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.tb_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_user.Location = new System.Drawing.Point(24, 79);
+            this.tb_user.Name = "tb_user";
+            this.tb_user.Size = new System.Drawing.Size(178, 26);
+            this.tb_user.TabIndex = 1;
+            // 
+            // tb_password
+            // 
+            this.tb_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.tb_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_password.Location = new System.Drawing.Point(24, 141);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(178, 26);
+            this.tb_password.TabIndex = 2;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(178)))), ((int)(((byte)(88)))));
+            this.btn_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(24, 205);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(178, 31);
+            this.btn_login.TabIndex = 3;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            // 
+            // F_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(224, 271);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_user);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "F_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ballance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_user;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
---
+++ View/F_Main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Ballance.View {
+    partial class F_Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.p_mainMenu = new System.Windows.Forms.Panel();
+            this.p_mainHeader = new System.Windows.Forms.Panel();
+            this.p_mainBody = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.p_mainMenu.SuspendLayout();
+            this.p_mainBody.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // p_mainMenu
+            // 
+            this.p_mainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.p_mainMenu.Controls.Add(this.panel1);
+            this.p_mainMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.p_mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.p_mainMenu.Name = "p_mainMenu";
+            this.p_mainMenu.Size = new System.Drawing.Size(168, 661);
+            this.p_mainMenu.TabIndex = 0;
+            // 
+            // p_mainHeader
+            // 
+            this.p_mainHeader.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.p_mainHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p_mainHeader.Location = new System.Drawing.Point(168, 0);
+            this.p_mainHeader.Name = "p_mainHeader";
+            this.p_mainHeader.Size = new System.Drawing.Size(816, 46);
+            this.p_mainHeader.TabIndex = 1;
+            // 
+            // p_mainBody
+            // 
+            this.p_mainBody.BackColor = System.Drawing.Color.White;
+            this.p_mainBody.Controls.Add(this.label1);
+            this.p_mainBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p_mainBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p_mainBody.Location = new System.Drawing.Point(168, 46);
+            this.p_mainBody.Name = "p_mainBody";
+            this.p_mainBody.Size = new System.Drawing.Size(816, 615);
+            this.p_mainBody.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(178)))), ((int)(((byte)(88)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(168, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bem Vindo !!";
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.p_mainBody);
+            this.Controls.Add(this.p_mainHeader);
+            this.Controls.Add(this.p_mainMenu);
+            this.Name = "F_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ballance";
+            this.Load += new System.EventHandler(this.F_Main_Load);
+            this.p_mainMenu.ResumeLayout(false);
+            this.p_mainBody.ResumeLayout(false);
+            this.p_mainBody.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel p_mainMenu;
+        private System.Windows.Forms.Panel p_mainHeader;
+        private System.Windows.Forms.Panel p_mainBody;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
